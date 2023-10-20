cf19f6fa9ab9da894ee6f731b713983191801a0e
Bharat V <bindian0509@gmail.com>
2023-10-20 23:59:59 +0530

    added user mapper dependencies

--- src/main/java/com/bharat/devops/user/mapper/UserMapper.java
+++ src/main/java/com/bharat/devops/user/mapper/UserMapper.java
@@ -1,7 +1,21 @@
 package com.bharat.devops.user.mapper;
+
+import com.bharat.devops.user.dto.UserDTO;
+
+import com.bharat.devops.user.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 /*
-* @author Bharat V. <bindian0509@gmail.com>
-* @created Thursday, 18 January 2024
-*/
-public class UserMapper {
+ * @author Bharat V. <bindian0509@gmail.com>
+ * @created Thursday, 18 January 2024
+ */
+@Mapper
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    User mapUserDTOToUser(UserDTO userDTO);
+    UserDTO mapUserToUserDTO(User user);
+
 }
+
