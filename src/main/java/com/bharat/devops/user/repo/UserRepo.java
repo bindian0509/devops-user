cc4ae378d298dace894020818bf8247a4be05783
Bharat V <bindian0509@gmail.com>
2023-10-22 23:59:59 +0530

    added user repo II

--- src/main/java/com/bharat/devops/user/repo/UserRepo.java
+++ src/main/java/com/bharat/devops/user/repo/UserRepo.java
@@ -1,8 +1,13 @@
 package com.bharat.devops.user.repo;
 
+import com.bharat.devops.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 /*
  * @author Bharat V. <bindian0509@gmail.com>
  * @created Thursday, 18 January 2024
  */
-public class UserRepo {
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
 }
